debbbb4b6692e72ba75e50f5723dc60fe5ab8223
Quentin Robertson <q.c.robertson@rgu.ac.uk>
2020-03-08 21:48:58 +0000

    menu additions and slight revisions

---
+++ SimFishy_Game/Assets/Scripts/MenuManager.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour {
+	
+
+	public void Quit () 
+	{
+		#if UNITY_EDITOR
+		UnityEditor.EditorApplication.isPlaying = false;
+		#else
+		Application.Quit();
+		#endif
+	}
+    public void Continue()
+    {
+        SceneManager.LoadScene("Scenes/GridTest");
+    }
+
+    public void NewGame()
+    {
+        SceneManager.LoadScene("Scenes/GridTest");
+    }
+
+}
